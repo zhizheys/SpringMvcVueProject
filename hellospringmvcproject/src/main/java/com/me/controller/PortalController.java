8fbb915853a464e2ba799ac03acfc559aa1b6c67
jliu14 <jean.liu@morningstar.com>
2018-09-03 17:54:10 +0800

    add web api hellospringmvcproject project

---
+++ hellospringmvcproject/src/main/java/com/me/controller/PortalController.java
@@ -0,0 +1,23 @@
+package com.me.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/portal")
+public class PortalController {
+	
+   @RequestMapping(value="/index",method=RequestMethod.GET)
+	public ModelAndView index() {
+	   ModelAndView modelAndView = new ModelAndView();
+	   modelAndView.setViewName("portal/index");
+	 
+
+	   return modelAndView;
+	}
+
+}
